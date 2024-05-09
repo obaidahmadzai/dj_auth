97b24222738718344f159ebb60a56da1f41d1d6c
obaidahmadzai <obaidullahkhan206@gmail.com>
2024-05-09 18:35:17 +0430

    user model update

--- backend/user/models.py
+++ backend/user/models.py
@@ -1,3 +1,18 @@
-from django.db import models
+# from django.db import models
+# from django.contrib.auth.models import AbstractUser
+# # Create your models here.
 
-# Create your models here.
+
+# class User(AbstractUser):
+#     class Role(models.TextChoices):
+#         ADMIN = "ADMIN", 'Admin'
+#         CUSTOMER = "CUSTOMER", 'Customer'
+
+#     base_role = Role.CUSTOMER
+
+#     role = models.CharField(max_length=50, choices=Role.choices)
+
+#     def save(self, *args, **kwargs):
+#         if not self.pk:
+#             self.role = self.base_role
+#             return super().save(*args, **kwargs)
